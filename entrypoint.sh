836454d60210cc184ea57602de45e41d22630580
fascenvi <85659367+fascenvi@users.noreply.github.com>
2021-06-25 16:06:17 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,10 @@ echo "aws version"
 
 aws --version
 
-echo "Attempting to update kubeconfig for aws"
+echo "Listing Clusters"
+aws eks list-clusters
 
+echo "Attempting to update kubeconfig for aws"
 aws eks --region "$INPUT_AWS_REGION" update-kubeconfig --name "$INPUT_CLUSTER_NAME"
+
 kubectl "$@"
