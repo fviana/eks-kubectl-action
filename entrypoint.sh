32ad655419f61c77bb39ffc326cc3278b48da469
fascenvi <85659367+fascenvi@users.noreply.github.com>
2021-06-25 16:11:07 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,14 @@ export AWS_ACCESS_KEY_ID="$INPUT_AWS_ACCESS_KEY_ID"
 export AWS_SECRET_ACCESS_KEY="$INPUT_AWS_SECRET_ACCESS_KEY"
 
 echo "aws version"
-
 aws --version
+
+echo "aws configure"
+
+aws configure set aws_access_key_id "$INPUT_AWS_ACCESS_KEY_ID"
+aws configure set aws_secret_access_key "$INPUT_AWS_SECRET_ACCESS_KEY"
+aws configure set region aws configure set region
+aws configure set role_arn "$INPUT_AWS_ROLE_ARN"
 
 echo "Listing Clusters"
 aws eks list-clusters
