16cfbea5f07054c5f68525ec6742192cd8062b53
fascenvi <85659367+fascenvi@users.noreply.github.com>
2021-06-28 10:24:59 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ echo "aws configure"
 aws configure set aws_access_key_id "$INPUT_AWS_ACCESS_KEY_ID"
 aws configure set aws_secret_access_key "$INPUT_AWS_SECRET_ACCESS_KEY"
 aws configure set region "$INPUT_AWS_REGION"
-aws configure set role_arn "$INPUT_AWS_ROLE_ARN"
+aws configure --profile eks-admin set role_arn "$INPUT_AWS_ROLE_ARN"
 aws configure --profile eks-admin set source_profile default
 aws configure --profile eks-admin set region "$INPUT_AWS_REGION"
 
