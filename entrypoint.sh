44d75c4d8010e2b6a3190270fdf73acfb6cd9eea
fascenvi <85659367+fascenvi@users.noreply.github.com>
2021-06-28 11:44:39 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,15 +12,6 @@ aws configure --profile eks-admin set role_arn "$INPUT_AWS_ROLE_ARN"
 aws configure --profile eks-admin set source_profile default
 aws configure --profile eks-admin set region "$INPUT_AWS_REGION"
 
-echo "cat ~/.aws/credentials"
-cat ~/.aws/credentials
-
-echo "cat ~/.aws/config"
-cat ~/.aws/config
-
-echo "Listing Clusters"
-aws eks list-clusters --profile eks-admin --region "$INPUT_AWS_REGION"
-
 echo "Attempting to update kubeconfig for aws"
 aws eks --region "$INPUT_AWS_REGION" update-kubeconfig --name "$INPUT_CLUSTER_NAME" --profile eks-admin
 
